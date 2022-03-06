0b8db47e2e26f534a01e68e29f86a447bd92b161
jitian <2207188023@qq.com>
2022-03-06 21:41:36 +0800

    在person中加了age属性

---
+++ src/com/hb/test/Preson.java
@@ -0,0 +1,11 @@
+package com.hb.test;
+
+/**
+ * @Auther: JiTian
+ * @Date: 2022/3/6 - 03 - 06 - 21:40
+ * @Description: com.hb.test
+ * @version: 1.0
+ */
+public class Preson {
+    private int age;
+}
